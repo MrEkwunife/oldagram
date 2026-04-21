1d03b6f0103ba7c75ce45b651c5fed16e7b5e179
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-04-21 20:30:24 +0100

    fix: remove handleLike from utils

--- src/utils.ts
+++ src/utils.ts
@@ -23,42 +23,5 @@ async function getPosts(): Promise<PostType[]> {
   return data;
 }
 
-function handleLike(index: number) {
-  const post = posts[index];
-
-  post.isLiked = !post.isLiked;
-  post.likes += post.isLiked ? 1 : -1;
-
-  localStorage.setItem("posts", JSON.stringify(posts));
-
-  const article = app.querySelectorAll("article")[index];
-  if (!article) return;
-
-  const likeBtn = article.querySelector(".like-btn img") as HTMLImageElement;
-  if (likeBtn) {
-    if (post.isLiked) {
-      likeBtn.src = likeBtn.src.includes("filled")
-        ? likeBtn.src
-        : likeBtn.src.replace(
-            "icon-heart",
-            "icon-filled-heart-removebg-preview",
-          );
-    } else {
-      likeBtn.src = likeBtn.src.replace(
-        "icon-filled-heart-removebg-preview",
-        "icon-heart",
-      );
-    }
-  }
-
-  const footer = article.querySelector(".post-footer") as HTMLElement;
-  if (footer) {
-    const likesText = footer.querySelector("p") as HTMLElement;
-    if (likesText) {
-      likesText.textContent = `${post.likes.toLocaleString()} likes`;
-    }
-  }
-}
-
 export { getImageSrc, getPosts };
 export type { PostType };
